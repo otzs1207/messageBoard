d5f8a481d3047b75fc4c0c99d1309e5f88480f49
yxd <yxd@example.com>
2017-12-26 20:16:57 +0800

    登录框

--- app/item/liveHall/controller/liveHallCtrl.js
+++ app/item/liveHall/controller/liveHallCtrl.js
@@ -5,6 +5,15 @@
         //活动 公告 新闻列表的json对象
         var that = this;
         console.log("加载成功...");
+        this.loginShow = false;
+        this.funcGetLoginUp = function(){
+            that.loginShow = true;
+        }
+
+        this.funcGetLoginUpClose = function(){
+            that.loginShow = false;
+            console.log(that.loginShow);
+        }
     }
 
     module.controller('xsWeb.liveHall.mainCtrl',['$scope', '$timeout', '$state','$interval','xsWeb.liveHall.mainSrv' ,liveHallCtrl])
